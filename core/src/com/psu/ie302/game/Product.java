52d77c413163b96b82cb2053553ec86db754ca6b
Nate <nate.crow76@gmail.com>
2016-03-23 01:04:30 -0400

    added Product class

---
+++ core/src/com/psu/ie302/game/Product.java
@@ -0,0 +1,55 @@
+package com.psu.ie302.game;
+
+public class Product {
+
+	private String name;
+	private String company;
+	private String description;
+	private float IRR;
+	private float MARR;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String prodName) {
+		this.name = prodName;
+	}
+	
+	public String getCompany() {
+		return this.company;
+	}
+	
+	public void setCompany(String prodCompany) {
+		this.company = prodCompany;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public void setDescription(String prodDesc) {
+		this.description = prodDesc;
+	}
+	
+	public float getIRR() {
+		return this.IRR;
+	}
+	
+	public void setIRR(float prodIRR) {
+		this.IRR = prodIRR;
+	}
+	
+	public float getMARR() {
+		return this.MARR;
+	}
+	
+	public void setMARR(float prodMARR) {
+		this.MARR = prodMARR;
+	}
+	
+	// randomly generate the IRR and MARR
+	public void generateIRRandMARR() {
+		;
+	}
+}
