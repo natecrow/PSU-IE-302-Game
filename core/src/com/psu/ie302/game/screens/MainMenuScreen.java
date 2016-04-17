ba2f8ff1bc643467aa74da70896baa0633593873
Nate <nate.crow76@gmail.com>
2016-04-17 18:41:16 -0400

    made initial GUI layout

--- core/src/com/psu/ie302/game/screens/MainMenuScreen.java
+++ core/src/com/psu/ie302/game/screens/MainMenuScreen.java
@@ -1,26 +1,52 @@
-package com.psu.ie302.game;
+package com.psu.ie302.game.screens;
 
 import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Json;
+import com.psu.ie302.game.IE302Game;
+import com.psu.ie302.game.Player;
+import com.psu.ie302.game.Product;
+import com.psu.ie302.game.ProductCalculations;
 import com.psu.ie302.game.questions.Question;
 import com.psu.ie302.game.questions.QuestionInflationType1;
 import com.psu.ie302.game.questions.QuestionInflationType2;
 import com.psu.ie302.game.questions.QuestionMultipleProducts;
 import com.psu.ie302.game.questions.QuestionSingleProduct;
-import com.psu.ie302.game.screens.AbstractScreen;
 
 public class MainMenuScreen extends AbstractScreen {
-
-	private Table table;
 	
 	public MainMenuScreen(final IE302Game game) {
 		super(game);
+		
+		// create table for the title
+		final Table tableTitle = new Table();
+		tableTitle.setPosition(0, IE302Game.VIRTUAL_HEIGHT / 2);
+		tableTitle.setSize(IE302Game.VIRTUAL_WIDTH, IE302Game.VIRTUAL_HEIGHT / 2);
+		stage.addActor(tableTitle);
+		tableTitle.setDebug(true);
+		
+		// create title label
+		//TODO: add bigger font to skin or create a title image to import
+		final Label labelTitle = new Label("PSU IE Shark Tank", game.skin);
+		labelTitle.setFontScale(3.0f);
+		labelTitle.setAlignment(Align.center);
+		
+		// add title label to its table
+		tableTitle.add(labelTitle).size(500f, 200f);
+		
+		// create table for menu buttons
+		final Table tableBtns = new Table();
+		tableBtns.setPosition(0, 0);
+		tableBtns.setSize(IE302Game.VIRTUAL_WIDTH, IE302Game.VIRTUAL_HEIGHT / 2);
+		stage.addActor(tableBtns);
+		tableBtns.setDebug(true);
 		
 		// create play button
 		final TextButton btnPlay;
@@ -29,18 +55,12 @@ public class MainMenuScreen extends AbstractScreen {
 		// create instructions button
 		final TextButton btnInstructions;
 		btnInstructions = new TextButton("Instructions", game.skin, "default");
-
-		// create table
-		table = new Table();
-		table.setFillParent(true);
-		stage.addActor(this.table);
-		table.setDebug(true);
 		
-		// size and add widgets to the table
-		table.defaults().size(200f, 50f);
-		table.add(btnPlay).space(10);
-		table.row();
-		table.add(btnInstructions).space(10);
+		// size and add menu buttons to their table
+		tableBtns.defaults().size(200f, 50f);
+		tableBtns.add(btnPlay).space(10);
+		tableBtns.row();
+		tableBtns.add(btnInstructions).space(10);
 		
 		// switch to a game screen when play button is clicked
 		btnPlay.addListener(new ChangeListener() {
@@ -73,15 +93,15 @@ public class MainMenuScreen extends AbstractScreen {
 		
 		// create array of questions
 		game.questions = new Question[8];
-		game.questions[1] = new QuestionSingleProduct(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)]);
+		game.questions[1] = new QuestionSingleProduct(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length - 1)]);
 		game.questions[0] = new QuestionInflationType1();
-		game.questions[2] = new QuestionSingleProduct(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)]);
+		game.questions[2] = new QuestionSingleProduct(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length - 1)]);
 		game.questions[3] = new QuestionInflationType1();
-		game.questions[4] = new QuestionMultipleProducts(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)],
+		game.questions[4] = new QuestionMultipleProducts(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length - 1)],
 				this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length - 1)],
 				3);
 		game.questions[5] = new QuestionInflationType2();
-		game.questions[6] = new QuestionMultipleProducts(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)],
+		game.questions[6] = new QuestionMultipleProducts(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length - 1)],
 						this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length - 1)],
 						3);
 		game.questions[7] = new QuestionInflationType2();
