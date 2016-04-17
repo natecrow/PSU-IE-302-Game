6069d58577136ca97a7199f9a49f1ab37e836529
Nate <nate.crow76@gmail.com>
2016-04-17 03:35:34 -0400

    added table to MainMenuScreen

--- core/src/com/psu/ie302/game/MainMenuScreen.java
+++ core/src/com/psu/ie302/game/MainMenuScreen.java
@@ -3,10 +3,10 @@ package com.psu.ie302.game;
 import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Json;
 import com.psu.ie302.game.questions.Question;
 import com.psu.ie302.game.questions.QuestionInflationType1;
@@ -17,38 +17,40 @@ import com.psu.ie302.game.screens.AbstractScreen;
 
 public class MainMenuScreen extends AbstractScreen {
 
-	final private TextButton btnPlay;
-	final private TextButton btnInstructions;
+	private Table table;
 	
 	public MainMenuScreen(final IE302Game game) {
-		
 		super(game);
 		
 		// create play button
-		this.btnPlay = new TextButton("Play", game.skin, "default");
-		this.btnPlay.setWidth(200f);
-		this.btnPlay.setHeight(30f);
-		this.btnPlay.setPosition(Gdx.graphics.getWidth()/2, (Gdx.graphics.getHeight()/2) + 20f, Align.center);
+		final TextButton btnPlay;
+		btnPlay = new TextButton("Play", game.skin, "default");
 		
 		// create instructions button
-		this.btnInstructions = new TextButton("Instructions", game.skin, "default");
-		this.btnInstructions.setWidth(200f);
-		this.btnInstructions.setHeight(30f);
-		this.btnInstructions.setPosition(Gdx.graphics.getWidth()/2, (Gdx.graphics.getHeight()/2) - 20f, Align.center);
+		final TextButton btnInstructions;
+		btnInstructions = new TextButton("Instructions", game.skin, "default");
+
+		// create table
+		table = new Table();
+		table.setFillParent(true);
+		stage.addActor(this.table);
+		table.setDebug(true);
+		
+		// size and add widgets to the table
+		table.defaults().size(200f, 50f);
+		table.add(btnPlay).space(10);
+		table.row();
+		table.add(btnInstructions).space(10);
 		
 		// switch to a game screen when play button is clicked
-		this.btnPlay.addListener(new ClickListener() {
+		btnPlay.addListener(new ChangeListener() {
 			@Override
-			public void clicked(InputEvent event, float x, float y) {
+			public void changed (ChangeEvent event, Actor btnPlay) {
 				initGameInfo();
 				switchToQuestionScreen(game);
 	            dispose();
 			}
 		});
-		
-		// add everything to stage
-		stage.addActor(this.btnPlay);
-		stage.addActor(this.btnInstructions);
 	}
 
 	/* 
@@ -57,7 +59,7 @@ public class MainMenuScreen extends AbstractScreen {
 	private void initGameInfo() {
 		
 		// create the player object
-		this.game.player = new Player();
+		game.player = new Player();
 		
 		// Read products from JSON file into an Array List
 		Json json = new Json();
@@ -66,35 +68,35 @@ public class MainMenuScreen extends AbstractScreen {
 				Gdx.files.local("products.json"));
 		
 		// Convert Array List of products into a regular array
-		this.game.products = new Product[productsList.size()];
-		this.game.products = productsList.toArray(this.game.products);
+		game.products = new Product[productsList.size()];
+		game.products = productsList.toArray(this.game.products);
 		
 		// create array of questions
-		this.game.questions = new Question[8];
-		this.game.questions[0] = new QuestionSingleProduct(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)]);
-		this.game.questions[1] = new QuestionInflationType1();
-		this.game.questions[2] = new QuestionSingleProduct(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)]);
-		this.game.questions[3] = new QuestionInflationType1();
-		this.game.questions[4] = new QuestionMultipleProducts(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)],
+		game.questions = new Question[8];
+		game.questions[1] = new QuestionSingleProduct(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)]);
+		game.questions[0] = new QuestionInflationType1();
+		game.questions[2] = new QuestionSingleProduct(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)]);
+		game.questions[3] = new QuestionInflationType1();
+		game.questions[4] = new QuestionMultipleProducts(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)],
 				this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)],
 				3);
-		this.game.questions[5] = new QuestionInflationType2();
-		this.game.questions[6] = new QuestionMultipleProducts(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)],
+		game.questions[5] = new QuestionInflationType2();
+		game.questions[6] = new QuestionMultipleProducts(this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)],
 						this.game.products[ProductCalculations.randomlyPickProduct(this.game.products.length)],
 						3);
-		this.game.questions[7] = new QuestionInflationType2();
+		game.questions[7] = new QuestionInflationType2();
 		
 		// display how much money player has so far
 		//System.out.println("Your starting amount: $" + this.game.player.getScore() + "\n");
 		
 		// set question iterator
-		this.game.qItr = 0;
+		game.qItr = 0;
 		
 	}
 
 	@Override
 	public void dispose() {
-		stage.dispose();
+		super.dispose();
 	}
 
 }
