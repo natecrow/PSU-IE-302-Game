b79b5e8c530f2ece5c5bfd836632646a2e927822
natecrow <nate.crow76@gmail.com>
2017-05-16 23:48:08 -0400

    Added Junit, specified dir structure for core in build

---
+++ core/src/test/java/com/psu/ie302/game/ProductTest.java
@@ -0,0 +1,24 @@
+package com.psu.ie302.game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProductTest {
+
+	@Test
+	public void testGenerateCashflows() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayCashflows() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayInitialInvestment() {
+		fail("Not yet implemented");
+	}
+
+}
