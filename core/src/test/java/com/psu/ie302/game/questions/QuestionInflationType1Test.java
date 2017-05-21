7200bcdae32d899b920bb4d0b3052c0703385bb8
natecrow <nate.crow76@gmail.com>
2017-05-20 20:25:20 -0400

    Added Powermock, created first successful test

---
+++ core/src/test/java/com/psu/ie302/game/questions/QuestionInflationType1Test.java
@@ -0,0 +1,56 @@
+package com.psu.ie302.game.questions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.math.BigDecimal;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.psu.ie302.game.Player;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ Player.class })
+public class QuestionInflationType1Test {
+
+	static QuestionInflationType1 question;
+
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		//@formatter:off
+		question = new QuestionInflationType1(
+				new BigDecimal(1000),
+				BigDecimal.valueOf(0.02f).setScale(4, BigDecimal.ROUND_HALF_UP),
+				10,
+				true);
+	}
+	
+	@Test
+	@Ignore
+	public void testSetQuestionPrompt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@Ignore
+	public void testSetCorrectAnswer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void whenPlayerAnswerIsCorrect_thenCheckAndDisplayAnswerResultsShouldReturnCorrect() {
+		Player mockedPlayer = mock(Player.class);
+		
+		assertEquals("CORRECT! ", question.checkAndDisplayAnswerResults("1218.99", mockedPlayer));
+		
+		verify(mockedPlayer).addScore(1);
+	}
+
+}
